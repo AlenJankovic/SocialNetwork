bbcd393f60de793643fbba658e68ca957fb2c4c3
AlenJankovic <alen.jankovic11@gmail.com>
2020-04-19 22:50:56 +0200

    Setting up Apache tiles Tiles definitions default.jsp tiles.xml

--- src/main/java/com/examen/App.java
+++ src/main/java/com/examen/App.java
@@ -2,14 +2,18 @@ package com.examen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+import org.springframework.web.servlet.view.tiles3.TilesConfigurer;
+import org.springframework.web.servlet.view.tiles3.TilesView;
 
 
-@Controller
-@EnableAutoConfiguration   
+
+@SpringBootApplication
 public class App extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
@@ -21,16 +25,27 @@ public class App extends SpringBootServletInitializer{
 		return application.sources(App.class);
 	}
 	
-	@RequestMapping("/")
-	String home() {
-		return "home";
+	@Bean
+	public TilesConfigurer tilesConfigurer() {
+		TilesConfigurer tilesConfigurer = new TilesConfigurer();
 		
+		String[] defs = {"/WEB-INF/tiles.xml"};  //	location of tiles.xml config file
+		
+		tilesConfigurer.setDefinitions(defs);
+		
+		return tilesConfigurer;
 	}
 	
-	@RequestMapping("/about")
-	String about() {
-		return "about";
-	}	
+	@Bean
+	public UrlBasedViewResolver tilesViewResolver() {
+		UrlBasedViewResolver tilesViewResolver = new UrlBasedViewResolver();
+		
+		tilesViewResolver.setViewClass(TilesView.class);
+		
+		return tilesViewResolver;
+	}
+	
+	
 }
 
 
---
+++ src/main/java/com/examen/controllers/PageController.java
@@ -0,0 +1,20 @@
+package com.examen.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/")
+	String home() {
+		return "app.homepage";
+		
+	}
+	
+	@RequestMapping("/about")
+	String about() {
+		return "about";
+	}	
+
+}
