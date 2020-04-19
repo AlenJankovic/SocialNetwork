464e8c477238dfc65c02413af41f573480f2e81d
AlenJankovic <alen.jankovic11@gmail.com>
2020-04-19 18:53:44 +0200

    Testing make home page and about page

---
+++ src/main/java/com/examen/App.java
@@ -0,0 +1,36 @@
+package com.examen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@EnableAutoConfiguration   
+public class App extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(App.class);
+	}
+	
+	@RequestMapping("/")
+	String home() {
+		return "home";
+		
+	}
+	
+	@RequestMapping("/about")
+	String about() {
+		return "about";
+	}	
+}
+
+
